b0d05c2fa1f5326cda8f25e3f3a9f342081d027b
Yang-Juseung <80092642+Yang-Juseung@users.noreply.github.com>
2022-06-13 20:48:20 +0900

    Add files via upload

--- CS230/Main.cpp
+++ CS230/Main.cpp
@@ -7,14 +7,16 @@ Project: CS230
 Author: Juseung Yang
 Creation date: 3/15/2022
 -----------------------------------------------------------------*/
-#include <stdexcept>	//std::exception
-#include <iostream>		//std::cerr
+#include <stdexcept>	
+#include <iostream>		
 
 #include "Engine/Engine.h"
 #include "Game/Splash.h"
 #include "Game/Mode1.h"
 #include "Game/Mode2.h"
+#include "Game/Mode3.h"
 #include "Game/Screens.h"
+#include "Game/Gameover.h"
 
 
 int main(void) 
@@ -26,11 +28,14 @@ int main(void)
 		Splash splash;
 		Mode1 mode1;
 		Mode2 mode2;
+		Mode3 mode3;
+		Game_end over;
 
 		engine.GetGameStateManager().AddGameState(splash);
 		engine.GetGameStateManager().AddGameState(mode1);
 		engine.GetGameStateManager().AddGameState(mode2);
-
+		engine.GetGameStateManager().AddGameState(mode3);
+		engine.GetGameStateManager().AddGameState(over);
 		while (engine.HasGameEnded() == false) 
 		{
 			engine.Update();
